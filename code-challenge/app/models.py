21963e52c3d2989e5db1f6f148810665fa6a0d22
faithwest <faithchepkoech425@gmail.com>
2024-01-17 08:39:06 +0300

    added validation

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,6 +1,8 @@
 # models.py
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+
 
 db = SQLAlchemy()
 
@@ -12,7 +14,6 @@ class Hero(db.Model):
     super_name = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
-
     powers = db.relationship('Power', secondary='hero_power')
 
 class Power(db.Model):
@@ -24,6 +25,10 @@ class Power(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
+    @validates('description')
+    def validate_description(self, key, description):
+        assert description and len(description) >= 20, "Description must be present and equal or more than 20 characters"
+        return description
 
 class Hero_Power(db.Model):
     __tablename__ = 'hero_power'
@@ -34,3 +39,8 @@ class Hero_Power(db.Model):
     strength = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        assert strength in ['Strong', 'Weak', 'Average'], f"Invalid strength: {strength}"
+        return strength
